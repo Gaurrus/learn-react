1a7b45c74beb0347495e25efd98affd55ba29020
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-12-09 19:35:01 +0300

    react 1

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Garrus learn react
-        </p>
-       <ul>
-         <li>1</li>
-         <li>2</li>
-         <li>3</li>
-       </ul>
+        <p>Garrus learn react</p>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
       </header>
     </div>
   );
