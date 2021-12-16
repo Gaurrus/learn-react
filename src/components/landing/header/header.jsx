b2912b5497d666f9403eb0fdd74eb8ffc16fc4de
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-12-16 22:00:41 +0300

    home03

---
+++ src/components/landing/header/header.jsx
@@ -0,0 +1,8 @@
+import styles from "../landing.module.css";
+
+export const Header =()=> (
+    <header className={styles.header}>
+        <div className={styles.orange}>logo</div>
+        <nav className={styles.nav}>nav</nav>
+    </header>
+)
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,22 +1,20 @@
 import styles from './landing.module.css'
+import {Header} from "./header";
 
 export const Landing = () => {
-  return (
-    <div className={styles.landing}>
-      <header className={styles.header}>
-        <div class={styles.orange}>logo</div>
-        <nav className={styles.nav}>nav</nav>
-      </header>
-      <main className={styles.main}>
-        <article className={styles.articleOne}>article</article>
-        <article className={styles.articleTwo}>article</article>
-        <aside className={styles.asideOne}>aside</aside>
-        <aside className={styles.asideTwo}>aside</aside>
-        <article className={styles.articleThree}>article</article>
-        <article className={styles.articleFour}>article</article>
-        <article className={styles.articleFive}>article</article>
-      </main>
-      <footer className={styles.footer}>footer</footer>
-    </div>
-  )
+    return (
+        <div className={styles.landing}>
+            <Header/>
+            <main className={styles.main}>
+                <article className={styles.articleOne}>article</article>
+                <article className={styles.articleTwo}>article</article>
+                <aside className={styles.asideOne}>aside</aside>
+                <aside className={styles.asideTwo}>aside</aside>
+                <article className={styles.articleThree}>article</article>
+                <article className={styles.articleFour}>article</article>
+                <article className={styles.articleFive}>article</article>
+            </main>
+            <footer className={styles.footer}>footer</footer>
+        </div>
+    )
 }
