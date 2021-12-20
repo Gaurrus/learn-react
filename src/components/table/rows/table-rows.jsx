15d1b3585704a4db154f4f0c04936b3d2d9061de
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-12-20 23:21:58 +0300

    закончил

--- src/components/table/rows/table-rows.jsx
+++ src/components/table/rows/table-rows.jsx
@@ -1,10 +1,17 @@
-import { inData } from '../../../constants/in-data';
-import styles from './table-rows.module.css'
+import styles from "./table-rows.module.css";
 
-export const TableRow = ({dataRow}, tr, th) => {
-  return <tr>
-    {dataRow.map((item, index) =>
-      <td key={index.toString()} className={styles.td}>{item}</td>
-    )}
+export const TableRow = ({ dataRow , th}) => (
+  <tr>
+    {dataRow.map((item, index) => {
+      return th ? (
+        <th key={index.toString()} className={styles.td}>
+          {item}
+        </th>
+      ) : (
+        <td key={index.toString()} className={styles.td}>
+          {item}
+        </td>
+      );
+    })}
   </tr>
-};
+);
