f3296c396d37a14b18d9c544c4e3b90ac7d638d4
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-12-23 22:31:44 +0300

    первый

--- src/components/app/app.jsx
+++
@@ -1,8 +0,0 @@
-import { Table } from '../table'
-import { List } from './../list';
-import './app.css'
-
-export const App = () => {
-  const isTableVisible = true;
-  return isTableVisible ? <Table /> : <List />
-}
