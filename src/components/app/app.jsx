bc30ea4a0a0feaea90085491a7faab8b0cd1d53e
Gaurrus <listenbulletformyvalentine@gmail.com>
2022-01-09 15:37:45 +0300

    ErrorBoundary

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,12 @@
-import { products } from "../../constants";
-
 import { Market } from "../market";
+import { ErrorBoundary } from './../error-boundary';
 
+import { products } from "../../constants";
 import "./app.module.css";
 
+
 export const App = (props) => (
-  <>
+  <ErrorBoundary>
     <Market products={products} />
-  </>
+  </ErrorBoundary>
 );
